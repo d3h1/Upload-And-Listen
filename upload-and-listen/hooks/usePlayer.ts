303aa250e01ce6bbb95706b5b80563752bbad508
denicabaravdic <denicabaravdic@gmail.com>
2023-07-09 15:29:10 -0700

    creating a song player

---
+++ upload-and-listen/hooks/usePlayer.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+interface PlayerStore {
+    ids: string[];
+    activeId?: string;
+    setId: (id: string) => void;
+    reset: () => void;
+}
+
+const usePlayer = create<PlayerStore>((set) => ({
+    // Everytime you hit play, you will set a list of ids which will play in a playlist and the current clicked id will be played 
+    ids: [],
+    activeId: undefined,
+    setId: (id: string) => set({ activeId: id }),
+    setIds: (ids: string[]) => set({ ids: ids }),
+    reset: () => set({ ids: [], activeId: undefined})
+}))
+
+export default usePlayer;
